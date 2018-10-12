1d405e7cae5a55751de88cabc6956850c6555251
JWLD <jackwldavies@icloud.com>
2018-10-12 21:32:27 +0100

    create global style component and render in App

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { hot } from 'react-hot-loader'
 import { Route, Switch } from 'react-router-dom'
 
 import { ArtistsScene } from 'scenes'
+import GlobalStyle from 'style/global'
 
 const App = () => (
-  <Switch>
-    <Route exact path="/" component={ArtistsScene} />
-  </Switch>
+  <Fragment>
+    <GlobalStyle />
+
+    <Switch>
+      <Route exact path="/" component={ArtistsScene} />
+    </Switch>
+  </Fragment>
 )
 
 export default hot(module)(App)
