817ec6c95084e37af9e5b072774f8e3752d384d9
JWLD <jackwldavies@icloud.com>
2018-10-12 21:38:39 +0100

    add some more global style rules

--- src/client/style/global.js
+++ src/client/style/global.js
@@ -1,8 +1,24 @@
 import { createGlobalStyle } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    border: 0;
+    border-radius: 0;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
   body {
     background-color: #21252B;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  a:hover, button:hover {
+    cursor: pointer;
   }
 `
 
