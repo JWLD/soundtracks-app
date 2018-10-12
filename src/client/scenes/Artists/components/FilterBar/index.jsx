4595dac427a8b37a03e596cf4cf33be4407ef955
JWLD <jackwldavies@icloud.com>
2018-10-12 20:14:25 +0100

    create basic FilterBar component

---
+++ src/client/scenes/Artists/components/FilterBar/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const FilterBar = () => <div>Filter Bar</div>
+
+export default FilterBar
--- src/client/scenes/Artists/index.jsx
+++ src/client/scenes/Artists/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
-const Landing = () => <div>Hello, world</div>
+import { FilterBar } from './components'
+
+const Landing = () => <FilterBar />
 
 export default Landing
