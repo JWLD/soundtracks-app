bf0a22f2a74b8e1205bdbfc4840f0f3749455ce8
JWLD <jackwldavies@icloud.com>
2018-11-21 18:20:08 +0000

    extract apollo setup code out to services folder

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { ApolloClient } from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
 import React from 'react'
 import { ApolloProvider } from 'react-apollo'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
-import App from 'App'
-import initialiseApp from 'helpers/initialise'
-import store from 'state/store'
+import App from './App'
+import client from './services/apollo'
+import initialiseApp from './helpers/initialise'
+import store from './state/store'
 
 initialiseApp()
-
-const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link: new HttpLink({ uri: 'http://localhost:4000/graphql' })
-})
 
 ReactDOM.render(
   <BrowserRouter>
