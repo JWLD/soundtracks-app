ef5dad2b436da1193775c0ca526c9986211ade4a
JWLD <jackwldavies@icloud.com>
2018-11-24 10:27:15 +0000

    add apollo-link-state to apollo setup

---
+++ src/services/apollo.js
@@ -0,0 +1,24 @@
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloClient } from 'apollo-client'
+import { ApolloLink } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { withClientState } from 'apollo-link-state'
+
+const cache = new InMemoryCache()
+
+const stateLink = withClientState({
+  cache,
+  defaults: { composersFilter: '' },
+  resolvers: {}
+})
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000/graphql'
+})
+
+const client = new ApolloClient({
+  cache,
+  link: ApolloLink.from([stateLink, httpLink])
+})
+
+export default client
