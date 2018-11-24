43b8f5c509d3e288c5495148fb94701604374204
JWLD <jackwldavies@icloud.com>
2018-11-24 21:12:06 +0000

    remove unnecessary Fragment element

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { hot } from 'react-hot-loader'
 import { Route, Switch } from 'react-router-dom'
 
@@ -7,14 +7,14 @@ import { Composer, Landing } from 'scenes'
 import GlobalStyle from 'style/global'
 
 const App = () => (
-  <Fragment>
+  <>
     <GlobalStyle />
 
     <Switch>
       <Route component={Landing} exact path={ROUTES.LANDING} />
       <Route component={Composer} path={`${ROUTES.COMPOSER}/:composerId`} />
     </Switch>
-  </Fragment>
+  </>
 )
 
 export default hot(module)(App)
