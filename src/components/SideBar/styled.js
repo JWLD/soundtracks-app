39e39450f7ff48062f76373447c0e94095df81a0
JWLD <jackwldavies@icloud.com>
2018-11-28 22:39:05 +0000

    remove unnecessary style property

--- src/components/SideBar/styled.js
+++ src/components/SideBar/styled.js
@@ -1,11 +1,10 @@
 import styled from 'styled-components'
 
-import { mixins, vars } from 'style'
+import { mixins } from 'style'
 
 export const SideBarWrap = styled.div`
   ${mixins.flex};
 
-  background-color: ${vars.color2};
   padding-left: 0.8rem;
   color: white;
   font-size: 2rem;
