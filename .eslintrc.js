6d32c0ca008f2e4a15e416dcc4f5e5e867b14e4f
jwld <jackwldavies@icloud.com>
2019-09-02 08:29:25 +0100

    Replace eslint config file

---
+++ .eslintrc.js
@@ -0,0 +1,59 @@
+const path = require('path')
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: 'babel-eslint',
+  plugins: [
+    'better-styled-components',
+    'sort-destructure-keys'
+  ],
+  rules: {
+    'better-styled-components/sort-declarations-alphabetically': 'error',
+    'import/namespace': ['error', {
+      allowComputed: true
+    }],
+    'import/no-named-as-default': 'off',
+    'no-console': 'off',
+    'object-shorthand': 'error',
+    'padding-line-between-statements': ['error', {
+      blankLine: 'always',
+      prev: '*',
+      next: ['return', 'try']
+    }],
+    'prettier/prettier': ['error', {
+      semi: false,
+      singleQuote: true
+    }],
+    'react/default-props-match-prop-types': 'error',
+    'react/display-name': 'off',
+    'react/jsx-sort-props': ['error', {
+      ignoreCase: true
+    }],
+    'react/no-unused-prop-types': 'error',
+    'react/sort-prop-types': ['error', {
+      ignoreCase: true
+    }],
+    'sort-keys': ['error', 'asc', {
+      caseSensitive: false,
+      natural: true
+    }],
+    'sort-destructure-keys/sort-destructure-keys': 'error'
+  },
+  settings: {
+    'import/resolver': 'webpack',
+    react: {
+      version: 'detect'
+    }
+  }
+}
