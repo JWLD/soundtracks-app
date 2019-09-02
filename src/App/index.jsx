c15004e182be139941d0d6bc206e2b39576a0fd0
jwld <jackwldavies@icloud.com>
2019-09-02 01:30:12 +0100

    Import hot from react-hot-loader/root

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { hot } from 'react-hot-loader'
+import { hot } from 'react-hot-loader/root'
 import { Route, Switch } from 'react-router-dom'
 
 import ROUTES from 'constants/routes'
@@ -25,4 +25,4 @@ const App = () => (
   </SC.PageWrap>
 )
 
-export default hot(module)(App)
+export default hot(App)
