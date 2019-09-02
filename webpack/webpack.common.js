5b1ecc1896699c96f445340364af5412da4d6914
jwld <jackwldavies@icloud.com>
2019-09-02 09:01:41 +0100

    Add html and clean plugins to webpack config

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,3 +1,5 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path')
 
 module.exports = {
@@ -16,6 +18,13 @@ module.exports = {
   output: {
     filename: 'bundle.js'
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      hash: true,
+      template: path.resolve(__dirname, '../public/index.html')
+    })
+  ],
   resolve: {
     extensions: ['.mjs', '.graphql', '.js', '.jsx'],
     modules: ['./src', 'node_modules']
