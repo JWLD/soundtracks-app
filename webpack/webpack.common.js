3e7afe050cbc497c1f3ec4e913088affb8e5342b
jwld <jackwldavies@icloud.com>
2019-09-02 08:37:44 +0100

    Create 3 webpack config files

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -22,8 +22,8 @@ module.exports = {
     }]
   },
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
   },
   plugins: [
     new BundleAnalyzerPlugin({
