32ec72f723dc63385c6238cfcd8c31ba2cf42258
jwld <jackwldavies@icloud.com>
2019-11-19 20:52:24 -0700

    Update albums query for new resolvers

--- src/scenes/Albums/helpers.js
+++ src/scenes/Albums/helpers.js
@@ -1,7 +1,7 @@
 import { checkStringForsubstring } from 'helpers/filtering'
 
-export const getAlbums = ({ artist, filter }) => {
-  return artist.albums.filter(album => {
+export const getAlbums = ({ albums, filter }) => {
+  return albums.filter(album => {
     return checkStringForsubstring({
       string: album.title,
       substring: filter
